b237bfcde43da76a2eedb0909ae3e1ff9a7af7df
kriptonian <shymal.sawan@gmail.com>
2022-06-09 17:15:56 +0530

    feat: Add Login page

--- src/resources/index.js
+++ src/resources/index.js
@@ -1,1 +1,4 @@
 export const boyImg = require("./fontBoy.png");
+export const googleImg = require("./googleImg.png");
+export const appleImg = require("./appleImg.png");
+export const facebookImg = require("./facebookImg.png");
